9ac23c1fca29995292668660034b795c55e31b4e
Chris McCafferty <cilefen@gmail.com>
2014-06-07 12:31:22 -0500

    Issue #2282063 by cilefen: Fixed window.MathJax configuration style breakes inline math.

--- src/Mathjax/Defaults.php
+++ src/Mathjax/Defaults.php
@@ -14,8 +14,6 @@ class Defaults {
   const CDNURL = 'https://c328740.ssl.cf1.rackcdn.com/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML';
   const CONFIG = "
 window.MathJax = {
-  extensions: ['tex2jax.js'],
-  jax: ['input/TeX','output/HTML-CSS'],
   tex2jax: {
     inlineMath: [ ['$','$'], ['\\\\(','\\\\)'] ],
     processEscapes: true,
