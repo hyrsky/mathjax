cca88bc693166b86ed5a41381ec91c7f1158d029
Chris McCafferty <cilefen@gmail.com>
2014-07-14 13:42:23 -0400

    Issue #2302939 by cilefen: Urgent, critical: update Mathjax CDN address.

--- src/Mathjax/Defaults.php
+++ src/Mathjax/Defaults.php
@@ -11,7 +11,7 @@ namespace Drupal\mathjax\Mathjax;
  * Contains default settings.
  */
 class Defaults {
-  const CDNURL = 'https://c328740.ssl.cf1.rackcdn.com/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML';
+  const CDNURL = 'https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML';
   const CONFIG = "
 window.MathJax = {
   tex2jax: {
