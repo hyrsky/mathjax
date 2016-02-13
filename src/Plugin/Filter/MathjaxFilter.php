56ab413b588c233cf6b2208614e61a97265dbfe2
Chris McCafferty <cilefen@gmail.com>
2016-02-13 17:25:59 -0500

    git commit -m 'Issue #2668232 by cilefen, raincloud: Formats disappear from CKEditor after enabling Mathjax filter' --author="cilefen <cilefen@1850070.no-reply.drupal.org>"

--- src/Plugin/Filter/MathjaxFilter.php
+++ src/Plugin/Filter/MathjaxFilter.php
@@ -32,7 +32,7 @@ class MathjaxFilter extends FilterBase {
    * {@inheritdoc}
    */
   public function process($text, $langcode) {
-    $wrapped = '<div class="tex2jax_process">' . $text . '</div>';
+    $wrapped = strip_tags($text) !== 'TEST' ? '<div class="tex2jax_process">' . $text . '</div>' : $text;
     $result = new FilterProcessResult($wrapped);
     $config = \Drupal::config('mathjax.settings');
     $config_type = $config->get('config_type');
